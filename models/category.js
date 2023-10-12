3bd62ebdb98b1768c35020f8b4f0cd63a34f3f0a
Juan Sanchez <js.sanchezd1@uniandes.edu.co>
2023-10-12 13:26:15 -0500

    Testing new version of categories

--- models/category.js
+++ models/category.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 const expenseSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 const Category = mongoose.model("Category", expenseSchema);
