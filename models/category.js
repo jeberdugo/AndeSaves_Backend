b8343fff1f9ea80265eea8b84ecbafae05c766ad
Juan Sanchez <js.sanchezd1@uniandes.edu.co>
2023-10-12 13:40:35 -0500

    adding id to categories

--- models/category.js
+++ models/category.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const expenseSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
